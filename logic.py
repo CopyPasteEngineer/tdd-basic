40bec93eaa15af63ed8af50adc2fad5d67680a84
SSripilaipong <SHSnail@mail.com>
2022-05-21 17:35:43 +0700

    feat: test should raise DrinkNotInMenu when buying cocoa

--- logic.py
+++ logic.py
@@ -5,5 +5,14 @@ class Cafe:
     }
 
     def buy(self, drink: str, money: float) -> (str, float):
+        self._validate_drink(drink)
         change = money - self.PRICE[drink]
         return drink, change
+
+    def _validate_drink(self, drink):
+        if drink not in self.PRICE:
+            raise DrinkNotInMenu()
+
+
+class DrinkNotInMenu(Exception):
+    pass
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,6 @@
-from logic import Cafe
+from pytest import raises
+
+from logic import Cafe, DrinkNotInMenu
 
 
 def test_should_return_tea_and_the_change_of_30_baht_when_buying_tea_with_50_baht():
@@ -11,3 +13,9 @@ def test_should_return_coffee_and_the_change_of_70_baht_when_buying_coffee_with_
     cafe = Cafe()
     drink, change = cafe.buy("coffee", 100.0)
     assert (drink, change) == ("coffee", 70.0)
+
+
+def test_should_raise_DrinkNotInMenu_when_buying_cocoa():
+    cafe = Cafe()
+    with raises(DrinkNotInMenu):
+        cafe.buy("cocoa", 50.0)
