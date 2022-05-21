b61becf3cdad9d33e2a07d6613db7683c1dd29be
SSripilaipong <SHSnail@mail.com>
2022-05-21 16:54:25 +0700

    feat: return coffee and the change of 70 baht when buying coffee with 100 baht

--- logic.py
+++ logic.py
@@ -1,3 +1,9 @@
 class Cafe:
+    PRICE = {
+        "tea": 20.0,
+        "coffee": 30.0,
+    }
+
     def buy(self, drink: str, money: float) -> (str, float):
-        return drink, money-20.0
+        change = money - self.PRICE[drink]
+        return drink, change
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -5,3 +5,9 @@ def test_should_return_tea_and_the_change_of_30_baht_when_buying_tea_with_50_bah
     cafe = Cafe()
     drink, change = cafe.buy("tea", 50.0)
     assert (drink, change) == ("tea", 30.0)
+
+
+def test_should_return_coffee_and_the_change_of_70_baht_when_buying_coffee_with_100_baht():
+    cafe = Cafe()
+    drink, change = cafe.buy("coffee", 100.0)
+    assert (drink, change) == ("coffee", 70.0)
