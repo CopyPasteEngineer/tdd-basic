0e0f39402deb208505593c5cb20e1f8db7122372
SSripilaipong <SHSnail@mail.com>
2022-05-21 17:39:56 +0700

    feat: should raise MoneyNotEnough when buying coffee with 1 baht

--- logic.py
+++ logic.py
@@ -6,8 +6,14 @@ class Cafe:
 
     def buy(self, drink: str, money: float) -> (str, float):
         self._validate_drink(drink)
-        change = money - self.PRICE[drink]
+        change = self._bill(drink, money)
         return drink, change
+
+    def _bill(self, drink, money):
+        change = money - self.PRICE[drink]
+        if change < 0:
+            raise MoneyNotEnough()
+        return change
 
     def _validate_drink(self, drink):
         if drink not in self.PRICE:
@@ -15,4 +21,8 @@ class Cafe:
 
 
 class DrinkNotInMenu(Exception):
+    pass
+
+
+class MoneyNotEnough(Exception):
     pass
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,6 +1,6 @@
 from pytest import raises
 
-from logic import Cafe, DrinkNotInMenu
+from logic import Cafe, DrinkNotInMenu, MoneyNotEnough
 
 
 def test_should_return_tea_and_the_change_of_30_baht_when_buying_tea_with_50_baht():
@@ -19,3 +19,9 @@ def test_should_raise_DrinkNotInMenu_when_buying_cocoa():
     cafe = Cafe()
     with raises(DrinkNotInMenu):
         cafe.buy("cocoa", 50.0)
+
+
+def test_should_raise_MoneyNotEnough_when_buying_coffee_with_1_baht():
+    cafe = Cafe()
+    with raises(MoneyNotEnough):
+        cafe.buy("coffee", 1.0)
