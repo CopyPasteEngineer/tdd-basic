26d0f7c587560d67d67859f7b592c4091b4c4f59
SSripilaipong <SHSnail@mail.com>
2022-05-22 16:12:02 +0700

    feat(app): should display drink and change when succeeded

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 class CafeApp:
 
-    def __init__(self, cafe):
+    def __init__(self, cafe, display):
         self._cafe = cafe
+        self._display = display
 
     def buy(self, drink: str, money: float):
-        self._cafe.buy(drink, money)
+        drink, change = self._cafe.buy(drink, money)
+        self._display(f"You bought {drink}. The change is {change:.1f} Baht. Thank you.")
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,14 +3,31 @@ from app import CafeApp
 
 def test_should_call_cafe_buy():
     cafe = CafeMock()
-    app = CafeApp(cafe=cafe)
+    app = CafeApp(cafe=cafe, display=DisplayMock())
     app.buy("tea", 30.0)
     assert cafe.buy_is_called_with_parameters == ("tea", 30.0)
 
 
-class CafeMock:
+def test_should_display_drink_and_change_when_succeeded():
+    display = DisplayMock()
+    app = CafeApp(cafe=CafeMock(return_value=("tea", 10.0)), display=display)
+    app.buy("tea", 30.0)
+    assert display.displayed_text == "You bought tea. The change is 10.0 Baht. Thank you."
+
+
+class DisplayMock:
     def __init__(self):
+        self.displayed_text = ""
+
+    def __call__(self, text):
+        self.displayed_text = text
+
+
+class CafeMock:
+    def __init__(self, return_value=None):
+        self._return_value = return_value or ("", 0.0)
         self.buy_is_called_with_parameters = ("", 0.0)
 
     def buy(self, drink: str, money: float) -> (str, float):
         self.buy_is_called_with_parameters = drink, money
+        return self._return_value
