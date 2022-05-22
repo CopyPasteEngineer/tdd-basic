16a49eb0222ad1c075190b2f7f44ef003a2da458
SSripilaipong <SHSnail@mail.com>
2022-05-22 14:05:13 +0700

    feat: should randomly select a drink between tea and coffee when buying surprise

--- logic.py
+++ logic.py
@@ -4,8 +4,11 @@ class Cafe:
         "coffee": 30.0,
     }
 
+    def __init__(self, randomly_select):
+        self._randomly_select = randomly_select
+
     def buy(self, drink: str, money: float) -> (str, float):
-        self._validate_drink(drink)
+        drink = self._validate_drink(drink)
         change = self._bill(drink, money)
         return drink, change
 
@@ -16,8 +19,12 @@ class Cafe:
         return change
 
     def _validate_drink(self, drink):
+        if drink == "surprise":
+            self._randomly_select(["tea", "coffee"])
+            drink = "coffee"
         if drink not in self.PRICE:
             raise DrinkNotInMenu()
+        return drink
 
 
 class DrinkNotInMenu(Exception):
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -4,24 +4,36 @@ from logic import Cafe, DrinkNotInMenu, MoneyNotEnough
 
 
 def test_should_return_tea_and_the_change_of_30_baht_when_buying_tea_with_50_baht():
-    cafe = Cafe()
+    cafe = Cafe(randomly_select=RandomlySelectMock())
     drink, change = cafe.buy("tea", 50.0)
     assert (drink, change) == ("tea", 30.0)
 
 
 def test_should_return_coffee_and_the_change_of_70_baht_when_buying_coffee_with_100_baht():
-    cafe = Cafe()
+    cafe = Cafe(randomly_select=RandomlySelectMock())
     drink, change = cafe.buy("coffee", 100.0)
     assert (drink, change) == ("coffee", 70.0)
 
 
 def test_should_raise_DrinkNotInMenu_when_buying_cocoa():
-    cafe = Cafe()
+    cafe = Cafe(randomly_select=RandomlySelectMock())
     with raises(DrinkNotInMenu):
         cafe.buy("cocoa", 50.0)
 
 
 def test_should_raise_MoneyNotEnough_when_buying_coffee_with_1_baht():
-    cafe = Cafe()
+    cafe = Cafe(randomly_select=RandomlySelectMock())
     with raises(MoneyNotEnough):
         cafe.buy("coffee", 1.0)
+
+
+def test_should_randomly_select_a_drink_between_tea_and_coffee_when_buying_surprise():
+    random_mock = RandomlySelectMock()
+    cafe = Cafe(randomly_select=random_mock)
+    cafe.buy("surprise", 100.0)
+    assert random_mock.is_called_with_choices == ["tea", "coffee"]
+
+
+class RandomlySelectMock:
+    def __call__(self, choices):
+        self.is_called_with_choices = choices
