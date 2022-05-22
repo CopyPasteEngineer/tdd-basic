a4931f908a705e53a5b52191e06c9d74e4053ff7
SSripilaipong <SHSnail@mail.com>
2022-05-22 16:19:12 +0700

    feat(app): should display message when DrinkNotInMenu is raised

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from logic import DrinkNotInMenu
+
+
 class CafeApp:
 
     def __init__(self, cafe, display):
@@ -5,5 +8,15 @@ class CafeApp:
         self._display = display
 
     def buy(self, drink: str, money: float):
-        drink, change = self._cafe.buy(drink, money)
+        try:
+            drink, change = self._cafe.buy(drink, money)
+        except DrinkNotInMenu:
+            self._display_drink_not_in_menu(drink)
+        else:
+            self._display_buy_success(change, drink)
+
+    def _display_buy_success(self, change, drink):
         self._display(f"You bought {drink}. The change is {change:.1f} Baht. Thank you.")
+
+    def _display_drink_not_in_menu(self, drink):
+        self._display(f"Sorry. We don't have {drink}.")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from app import CafeApp
+from logic import DrinkNotInMenu
 
 
 def test_should_call_cafe_buy():
@@ -15,6 +16,13 @@ def test_should_display_drink_and_change_when_succeeded():
     assert display.displayed_text == "You bought tea. The change is 10.0 Baht. Thank you."
 
 
+def test_should_display_message_when_DrinkNotInMenu_is_raised():
+    display = DisplayMock()
+    app = CafeApp(cafe=CafeMock(error=DrinkNotInMenu()), display=display)
+    app.buy("cocoa", 100.0)
+    assert display.displayed_text == "Sorry. We don't have cocoa."
+
+
 class DisplayMock:
     def __init__(self):
         self.displayed_text = ""
@@ -24,10 +32,13 @@ class DisplayMock:
 
 
 class CafeMock:
-    def __init__(self, return_value=None):
+    def __init__(self, return_value=None, error=None):
         self._return_value = return_value or ("", 0.0)
+        self._error = error
         self.buy_is_called_with_parameters = ("", 0.0)
 
     def buy(self, drink: str, money: float) -> (str, float):
         self.buy_is_called_with_parameters = drink, money
+        if self._error:
+            raise self._error
         return self._return_value
